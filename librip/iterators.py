1da198ce686c0212b04f445c83e7266c3e4c328c
irnef <43137024+irnef@users.noreply.github.com>
2019-10-27 14:34:32 +0300

    Update iterators.py

--- librip/iterators.py
+++ librip/iterators.py
@@ -1,17 +1,48 @@
+@@ -0,0 +1,47 @@
 # Итератор для удаления дубликатов
 class Unique(object):
     def __init__(self, items, **kwargs):
-        # Нужно реализовать конструктор
-        # В качестве ключевого аргумента, конструктор должен принимать bool-параметр ignore_case,
-        # в зависимости от значения которого будут считаться одинаковые строки в разном регистре
-        # Например: ignore_case = True, Aбв и АБВ разные строки
-        #           ignore_case = False, Aбв и АБВ одинаковые строки, одна из них удалится
-        # По-умолчанию ignore_case = False
-        pass
+        if type(items) is list:
+            self.items = items
+        else:
+            self.generated_items = [i for i in items]
+            self.items = self.generated_items
+
+        self.limit = len(self.items)
+        self.current = 0
+
+        self.ignore_case = kwargs.get('ignore_case')
+
+        if self.ignore_case:
+            self.unique_dict = dict()
+        else:
+            self.unique = set()
 
     def __next__(self):
-        # Нужно реализовать __next__    
-        pass
+        if self.ignore_case:
+            while self.items[self.current].lower() in self.unique_dict:
+                if self.current < self.limit:
+                    self.current += 1
+
+                    if self.current >= self.limit:
+                        raise StopIteration
+                else:
+                    break
+
+            self.unique_dict.update({self.items[self.current].lower(): self.items[self.current]})
+            return self.items[self.current]
+        else:
+            while self.items[self.current] in self.unique:
+                if self.current < self.limit:
+                    self.current += 1
+
+                    if self.current >= self.limit:
+                        raise StopIteration
+                else:
+                    break
+
+            self.unique.add(self.items[self.current])
+            return self.items[self.current]
 
     def __iter__(self):
         return self

5c5d4ad9ef9cd418de75445a74183fffeae091e8
irnef <43137024+irnef@users.noreply.github.com>
2019-10-27 14:27:32 +0300

    Update ex_2.py

--- ex_2.py
+++ ex_2.py
@@ -1,8 +1,36 @@
-#!/usr/bin/env python3
+@@ -0,0 +1,35 @@
+from __future__ import print_function
+
 from librip.gens import gen_random
 from librip.iterators import Unique
 
-data1 = [1, 1, 1, 1, 1, 2, 2, 2, 2, 2]
-data2 = gen_random(1, 3, 10)
 
-# Реализация задания 2
+def test_unique_iterator():
+    print('TEST WITH LIST OF INTS')
+    data1 = [1, 1, 1, 1, 1, 2, 2, 2, 2, 2]
+    print(data1)
+    for i in Unique(data1, ignore_case=False):
+        print(i, end=' ')
+    print('\n')
+
+    print('TEST WITH GENERATOR OF INTS\ngen_random(1, 3, 10)')
+    data2 = gen_random(1, 3, 10)
+    for i in Unique(data2, ignore_case=False):
+        print(i, end=' ')
+    print('\n')
+
+    print('TEST WITH LIST OF STR (ignore_case=False)')
+    data3 = ['a', 'A', 'b', 'B']
+    print(data3)
+    for i in Unique(data3, ignore_case=False):
+        print(i, end=' ')
+    print('\n')
+
+    print('TEST WITH LIST OF STR (ignore_case=True)')
+    print(data3)
+    for i in Unique(data3, ignore_case=True):
+        print(i, end=' ')
+
+
+if __name__ == "__main__":
+    test_unique_iterator()
